470f14db74278db3700d9a1eecef0200850a722c
TabasshinAfrinMeghla <tabasshinafrinmeghla@gmail.com>
2024-09-20 18:38:48 +0600

    add

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,18 +1,35 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import Nav from "./Nav";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container max-auto">
+      <div className="container max-auto flex justify-between items-center">
 
-{/* {logo} */}
+        {/* {logo} */}
 
         <Link href="/">
-        <h1 className="text-4xl font-semibold">
-          Luke <span>.</span>
-        </h1>
+          <h1 className="text-4xl font-semibold">
+            Meghla <span className=" text-neutral-700">.</span>
+          </h1>
         </Link>
+        {/*desktop nav*/}
+
+        <div className="hidden xl:flex  gap-8">
+
+          {/* <Nav></Nav> */}
+          <Nav></Nav>
+
+          <Link href='/contact'>
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+
+        {/* mobile nav */}
+
+        <div className="xl:hidden">Mobile nav</div>
+
       </div>
     </header>
   )
---
+++ components/Nav.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const link = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  }
+]
+
+
+const Nav = () => {
+  return (
+    <nav className="flex gap-8 ">
+
+      {link.map((link, index) => {
+        return <Link href={link.path} key={index} >{link.name} </Link>
+      })}
+
+    </nav>
+  )
+}
+
+export default Nav
