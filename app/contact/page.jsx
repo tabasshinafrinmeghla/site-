57f8ceb262d7d3cf2b52c7ea7fc036c2a97ac624
TabasshinAfrinMeghla <tabasshinafrinmeghla@gmail.com>
2024-09-21 23:18:20 +0600

    error solve

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Contact = () => {
   return (
+    
     <div>
       contact page
+      
     </div>
   )
 }
--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -1,23 +1,36 @@
 "use client";
 
-import { AnimatePresence,motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion";
+
 import { usePathname } from "next/navigation";
+
+
 
 const PageTransition = ({ children }) => {
 
+
   const pathname = usePathname();
-  return
+  return(
   <AnimatePresence>
     <div key={pathname}>
-      <motion.div initial={{opacity: 1}} animate={{opacity:0, transition: {delay:1, duration:0.4, ease:"easeInOut" }}}
-      className="h-screen w-screen fixed bg-slate-600 top-8 pointer-events-none"
-      />
-      {children}
+      <motion.div initial={{ opacity: 1 }} animate={{
+        opacity: 0, transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+      }}
+        className="h-screen w-screen fixed bg-slate-600 top-8 pointer-events-none"
+      >
+
+      </motion.div>
+
+
     </div>
+
+    { children }
 
   </AnimatePresence>
 
+ 
 
+    );
 
 };
 
